d16d64741293183dda73868de41085b3c053c9df
latrisha <l_gomer@yahoo.com>
2025-03-16 21:01:29 -0700

    fix: correct the foodSchema, update the index template

--- controllers/foods.js
+++ controllers/foods.js
@@ -21,4 +21,16 @@ router.get('/new', async (req, res) => {
     res.render('foods/new.ejs');
 });
 
+router.post('/', async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.session.user._id);
+      currentUser.foods.push(req.body);
+      await currentUser.save();
+      res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (error) {
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+
 module.exports = router;
